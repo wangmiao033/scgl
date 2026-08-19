987937370af696866cad457962daa85d60744f1d
wangmiao033 <161289342+wangmiao033@users.noreply.github.com>
2026-08-19 11:02:17 +0800

    Mount migrated Today Updates workbench

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import { WorkbenchInjector } from "@/components/workbench-injector";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <WorkbenchInjector />
         <Toaster
           position="top-right"
           toastOptions={{
